68eb1554345b080cfa75f01ee653620fad96701c
Michael Sogos <michael.sogos@gurustudioweb.it>
2020-08-22 00:09:05 +0200

    * Improved login exception handler

--- src/modules/Auth/auth.service.ts
+++ src/modules/Auth/auth.service.ts
@@ -10,43 +10,47 @@ export class AuthService {
     constructor(private readonly repositoryService: TenantRepositoryService, private readonly tenantService: TenantManagerService) {}
 
     async login(tenantAlias: string, credentials: { username: string; password: string }): Promise<AuthTokenPayload> {
-        const tenant = this.tenantService.getTenantConfig(tenantAlias);
-        const authConfig: LocalAuthConfig = tenant.tenant_auth_config.auth_config;
+        try {
+            const tenant = this.tenantService.getTenantConfig(tenantAlias);
+            const authConfig: LocalAuthConfig = tenant.tenant_auth_config.auth_config;
 
-        let user: User = null;
-        switch (tenant.tenant_auth_config.auth_type) {
-            case 'local':
-                {
-                    user = await this.localAuthenticate(tenantAlias, credentials);
-                }
-                break;
-            case 'oauth2':
-                throw new NotImplementedException();
-            case 'saml2':
-                throw new NotImplementedException();
+            let user: User = null;
+            switch (tenant.tenant_auth_config.auth_type) {
+                case 'local':
+                    {
+                        user = await this.localAuthenticate(tenantAlias, credentials);
+                    }
+                    break;
+                case 'oauth2':
+                    throw new NotImplementedException();
+                case 'saml2':
+                    throw new NotImplementedException();
+            }
+
+            const access_token = jwt.sign(Object.assign({}, this.createUserProfileFromUser(user)), authConfig.secretKey, {
+                audience: [`tenant:${tenant.code}`],
+                issuer: 'primebrick',
+                expiresIn: authConfig.tokenExpiresIn || '15m',
+            });
+
+            const refresh_token = jwt.sign({}, authConfig.secretKey, {
+                audience: [`tenant:${tenant.code}`],
+                issuer: 'primebrick',
+                expiresIn: authConfig.sessionExpiresIn || '1d',
+            });
+
+            const access_token_payload = jwt.decode(access_token);
+
+            const payload = new AuthTokenPayload();
+            payload.access_token = access_token;
+            payload.refresh_token = refresh_token;
+            payload.token_type = 'Bearer';
+            payload.expires_in = (access_token_payload['exp'] as number) - (access_token_payload['iat'] as number);
+
+            return payload;
+        } catch (ex) {
+            throw new UnauthorizedException('Credentials are invalid!');
         }
-
-        const access_token = jwt.sign(Object.assign({}, this.createUserProfileFromUser(user)), authConfig.secretKey, {
-            audience: [`tenant:${tenant.code}`],
-            issuer: 'primebrick',
-            expiresIn: authConfig.tokenExpiresIn || '15m',
-        });
-
-        const refresh_token = jwt.sign({}, authConfig.secretKey, {
-            audience: [`tenant:${tenant.code}`],
-            issuer: 'primebrick',
-            expiresIn: authConfig.sessionExpiresIn || '1d',
-        });
-
-        const access_token_payload = jwt.decode(access_token);
-
-        const payload = new AuthTokenPayload();
-        payload.access_token = access_token;
-        payload.refresh_token = refresh_token;
-        payload.token_type = 'Bearer';
-        payload.expires_in = (access_token_payload['exp'] as number) - (access_token_payload['iat'] as number);
-
-        return payload;
     }
 
     async refreshToken(tenantAlias: string, refreshToken: string, accessToken: string): Promise<AuthTokenPayload> {
