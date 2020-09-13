09fc4ed97a8937e5d0ea7c409d6734657d20c19e
Michael Sogos <michael.sogos@gurustudioweb.it>
2020-09-13 19:52:00 +0200

    * Fixed "production" script * Added newer translations * Fixed issue cause refactoring

--- test/app.controller.spec.ts
+++ test/app.controller.spec.ts
@@ -1,22 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from '../src/app.controller';
+import { AppSecureController } from '../src/app.secure.controller';
 import { AppService } from '../src/app.service';
 
 describe('AppController', () => {
-    let appController: AppController;
+    let appController: AppSecureController;
 
     beforeEach(async () => {
         const app: TestingModule = await Test.createTestingModule({
-            controllers: [AppController],
+            controllers: [AppSecureController],
             providers: [AppService],
         }).compile();
 
-        appController = app.get<AppController>(AppController);
+        appController = app.get<AppSecureController>(AppSecureController);
     });
 
     describe('root', () => {
         it('should return "Hello World!"', () => {
-            expect(appController.seedDatabase('local', 'notExistingModule')).toBe('Hello World!');
+            expect(appController.seedDatabase('local')).toBe('Hello World!');
         });
     });
 });
