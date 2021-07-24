1299cfcfc21fd4b172c6033c71bcc85fe9af2a46
Michael Sogos <michael.sogos@gurustudioweb.it>
2021-07-24 15:02:32 +0200

    * Moved logic to retrieve translation from handmade query to a call of core microservice * Added endpoints to retrieve app manifest and side menu items

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,27 +1,52 @@
 import { Controller, Get, Query } from '@nestjs/common';
-import { QueryPayload, QueryResult } from 'primebrick-sdk';
-import { QueryFilterOperator } from 'primebrick-sdk/dist/modules/MicroserviceManager/models/QueryPayload';
-import { DataAccessService } from './modules/DataAccess/dataaccess.service';
+import { GlobalRpcAction, ProcessorManagerService } from 'primebrick-sdk';
 
 @Controller('api')
 export class AppController {
-    constructor(private readonly dataAccessService: DataAccessService) {}
+    constructor(private readonly processorManagerService: ProcessorManagerService) {}
 
     @Get('meta/translations')
-    async getTranslations(@Query('languageCode') languageCode: string, @Query('group') translationGroup: string): Promise<QueryResult> {
-        const query = new QueryPayload();
-        query.brick = 'core';
-        query.entity = 'MetaTranslation';
-        query.fields = ['key', 'languageCode', 'value', 'group', 'isTemplate'];
-        query.filters = [
-            {
-                expressions: ['$self.languageCode = :languageCode ', '$self.group = :translationGroup'],
-                expressionValues: { languageCode, translationGroup },
-                expressionOperator: QueryFilterOperator.AND,
-                leftOperator: QueryFilterOperator.AND,
-            },
-        ];
+    async getTranslations(
+        // @Req() request: Request,
+        // @Query('languageCode') languageCode: string,
+        @Query('group') group: string,
+    ): Promise<any[]> {
+        // const browserLanguage = request.acceptsLanguages().filter((s) => !s.includes('*'));
+        // if (!languageCode && browserLanguage.length > 0) languageCode = browserLanguage[0].split('-')[0];
+        // if (!languageCode) throw new PreconditionFailedException("Missing query parameter 'languageCode'!");
+        // if (!translationGroup) throw new PreconditionFailedException("Missing query parameter 'group'!");
 
-        return await this.dataAccessService.findMany(query);
+        // const query = new QueryPayload();
+        // query.brick = 'core';
+        // query.entity = 'MetaTranslation';
+        // query.fields = ['key', 'languageCode', 'value', 'group', 'isTemplate'];
+        // query.filters = [
+        //     {
+        //         expressions: ['$self.languageCode = :languageCode ', '$self.group = :translationGroup'],
+        //         expressionValues: { languageCode, translationGroup },
+        //         expressionOperator: QueryFilterOperator.AND,
+        //         leftOperator: QueryFilterOperator.AND,
+        //     },
+        // ];
+
+        // const queryResult = await this.dataAccessService.findMany(query);
+
+        // if (queryResult.count <= 0) query.filters[0].expressionValues['languageCode'] = 'en';
+        // else return queryResult;
+
+        // return await this.dataAccessService.findMany(query);
+
+        let response = await this.processorManagerService.sendMessage<{ group: string; languageCode?: string }, any[]>(
+            GlobalRpcAction.GET_TRANSLATION,
+            { group },
+        );
+        if (response.data.length <= 0)
+            response = await this.processorManagerService.sendMessage<{ group: string; languageCode: string }, any[]>(
+                GlobalRpcAction.GET_TRANSLATION,
+                { group, languageCode: 'en' },
+            );
+        else return response.data;
+
+        return response.data;
     }
 }
--- src/app.secure.controller.ts
+++ src/app.secure.controller.ts
@@ -1,21 +1,33 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Migration } from 'typeorm';
-import { AuthGuard } from 'primebrick-sdk';
+import { AppManifest, AuthGuard, GlobalRpcAction, ProcessorManagerService } from 'primebrick-sdk';
 
-@Controller('api/management')
+@Controller('api')
 @UseGuards(AuthGuard)
 export class AppSecureController {
     //TODO: @mso -> Remove di controller in order to call core-brick service to run specific brick method (reponsability concern)
-    constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService, private readonly processorManagerService: ProcessorManagerService) {}
 
-    @Post('updatedb')
+    @Post('management/updatedb')
     async updateDatabase(): Promise<Migration[]> {
         return await this.appService.updateDatabaseSchema();
     }
 
-    @Post('seeddb')
+    @Post('management/seeddb')
     async seedDatabase(): Promise<any[]> {
         return await this.appService.importDataInitData();
+    }
+
+    @Get('meta/info')
+    async getAppInfo(): Promise<AppManifest> {
+        const response = await this.processorManagerService.sendMessage<null, AppManifest>(GlobalRpcAction.GET_APP_MANIFEST, null);
+        return response.data;
+    }
+
+    @Get('meta/menu')
+    async getAppMenu(): Promise<any[]> {
+        const response = await this.processorManagerService.sendMessage<null, any[]>(GlobalRpcAction.GET_SIDEMENU, null);
+        return response.data;
     }
 }
